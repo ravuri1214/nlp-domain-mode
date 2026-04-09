c94c8f24be3f9e7896408632ed004c7910f88030
Anusha R <ranusha0429@gmail.com>
2026-04-08 21:28:26 -0400

    Update predict.py

--- app/predict.py
+++ app/predict.py
@@ -1,1 +1,27 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from data import texts, labels
 
+
+def train_for_inference():
+    vectorizer = TfidfVectorizer()
+    X = vectorizer.fit_transform(texts)
+
+    model = LogisticRegression()
+    model.fit(X, labels)
+
+    return model, vectorizer
+
+
+def predict_label(text: str):
+    model, vectorizer = train_for_inference()
+    text_vector = vectorizer.transform([text])
+    prediction = model.predict(text_vector)
+    return prediction[0]
+
+
+if __name__ == "__main__":
+    sample_text = "customer unable to login"
+    predicted_label = predict_label(sample_text)
+    print(f"Input: {sample_text}")
+    print(f"Predicted Label: {predicted_label}")
